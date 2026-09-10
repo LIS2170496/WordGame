43bda1daa871271a47f05eafbf33ecbf2502bea9
LIS2170496 <LIS2170496@maricopa.edu>
2026-09-09 18:31:33 -0700

    Removed Person player1 from main, replaced with Person person created in GamePlay

--- GamePlay.java
+++ GamePlay.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class GamePlay {
 
     //Requirement 5a - private Person field
-    private Person person;
+    //Updated to create new Person at this level 9.9.2026  
+    private Person person = new Person();
+
 
 
     public static void main(String[] args) {
@@ -11,7 +13,9 @@ public class GamePlay {
         String personFirstName;
         String personLastName;
         String nameDecision;
-        Person player1;
+
+        //New game 9.9.2026
+        GamePlay myGame = new GamePlay();
 
 
         //Requirement 5b - ask for person's name in main
@@ -32,18 +36,21 @@ public class GamePlay {
 
         
         //Requirement 5ci & 5cii - Instance of Person depending on fname or fname+lname
+        //Updated player1 to myGame.person  9.9.2026
         if (nameDecision.equals("Y")) {
             System.out.println("Enter your last name: ");
             personLastName = scan.nextLine();
-            player1 = new Person(personFirstName, personLastName);
+            myGame.person.setFirstName(personFirstName);
+            myGame.person.setLastName(personLastName);
         }
         else {
-            player1 = new Person(personFirstName);
+            myGame.person.setFirstName(personFirstName);
         }
         
 
         //Welcome message to confirm what is stored in Person object
-        System.out.println("Welcome, " + player1.getFullName() );
+        //Updated player1 to myGame.person  9.9.2026
+        System.out.println("Welcome, " + myGame.person.getFullName() );
 
 
         //Requirement 5d - new Numbers instance that generates a random number
@@ -55,7 +62,7 @@ public class GamePlay {
         boolean guessCorrect = false;
 
         while (!guessCorrect) {
-            System.out.println(player1.getFullName() + ", guess my random number between 0 and 100");
+            System.out.println(myGame.person.getFullName() + ", guess my random number between 0 and 100");
             playerGuess = scan.nextInt();
             guessCorrect = aRandomNumber.compareNumber(playerGuess);
         }
