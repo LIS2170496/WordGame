d135a9eefca871d04bdb07a3ba2dcef1dd23c0f9
LIS2170496 <LIS2170496@maricopa.edu>
2026-09-11 15:17:06 -0700

    New method to return Numbers object

--- Hosts.java
+++ Hosts.java
@@ -1,8 +1,13 @@
 public class Hosts extends Person {
+    Numbers newNumber;
 
     //Requirement 5a - instantiate Numbers.java and generate a random number
     public void randomizeNum() {
         Numbers newNumber = new Numbers();
         newNumber.generateNumber();
+    }
+
+    public Numbers getNumber() {
+        return newNumber;
     }
 }
