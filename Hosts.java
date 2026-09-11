c53ec730f6297097fe96324cb3d9463f73d334e2
LIS2170496 <LIS2170496@maricopa.edu>
2026-09-11 14:14:46 -0700

    Created Hosts class with randNum generation

---
+++ Hosts.java
@@ -0,0 +1,8 @@
+public class Hosts extends Person {
+
+    //Requirement 5a - instantiate Numbers.java and generate a random number
+    public void randomizeNum() {
+        Numbers newNumber = new Numbers();
+        newNumber.generateNumber();
+    }
+}
