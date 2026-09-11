e6b78638bd66a20b2aceea3f1c120fd593e97b7b
LIS2170496 <LIS2170496@maricopa.edu>
2026-09-11 16:43:18 -0700

    overhaul Person to Players, add new Hosts, add game play while loop to use takeTurn in Turn class

--- GamePlay.java
+++ GamePlay.java
@@ -4,23 +4,34 @@ public class GamePlay {
 
     //private Person field
     //Updated to create new Person at this level 9.9.2026  
-    private Person person = new Person();
+    //Requirement 7b - Person person is now a Players player
+    private Players player = new Players();
 
 
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String personFirstName;
-        String personLastName;
+
+        //Change variables to refer to player instead of person
+        String playerFirstName;
+        String playerLastName;
         String nameDecision;
 
         //New game 9.9.2026
         GamePlay myGame = new GamePlay();
 
 
-        //ask for person's name in main
+        //Requirement 7a - instantiate Host and generate random number
+        Hosts bobBarker = new Hosts("Bob", "Barker");
+        bobBarker.randomizeNum();
+
+
+
+
+
+        //ask for player's name in main
         System.out.println("Player 1, enter your first name: ");
-        personFirstName = scan.nextLine();
+        playerFirstName = scan.nextLine();
 
         //ask if they want to add last name
         System.out.println("Would you like to add your last name? (Y / N)");
@@ -35,37 +46,63 @@ public class GamePlay {
 
 
         
-        //Instance of Person depending on fname or fname+lname
+        //Constructor for Person depending on fname or fname+lname
         //Updated player1 to myGame.person  9.9.2026
+        //Updated myGame.person to myGame.player 9.11.2026
         if (nameDecision.equals("Y")) {
             System.out.println("Enter your last name: ");
-            personLastName = scan.nextLine();
-            myGame.person.setFirstName(personFirstName);
-            myGame.person.setLastName(personLastName);
+            playerLastName = scan.nextLine();
+            myGame.player.setFirstName(playerFirstName);
+            myGame.player.setLastName(playerLastName);
         }
         else {
-            myGame.person.setFirstName(personFirstName);
+            myGame.player.setFirstName(playerFirstName);
         }
         
 
         //Welcome message to confirm what is stored in Person object
         //Updated player1 to myGame.person  9.9.2026
-        System.out.println("Welcome, " + myGame.person.getFullName() );
+        //Updated myGame.person to myGame.player 9.11.2026
+        System.out.println("Welcome, " + myGame.player.getFullName() );
 
 
+
+        
+
+
+        //Requirement 7c - removed this section
         //new Numbers instance that generates a random number
-        Numbers aRandomNumber = new Numbers();
-        aRandomNumber.generateNumber();
+        //Numbers aRandomNumber = new Numbers();
+        //aRandomNumber.generateNumber();
 
+
+        //Requirement 7d - instantiate Turn
+        Turn newTurn = new Turn();
+
+
+        
+        
+
+
+        //Requirement 7e - move this prompt to takeTurn method
+        //int playerGuess;
+        //boolean guessCorrect = false;
+        //while (!guessCorrect) {
+        //    System.out.println(myGame.player.getFullName() + ", guess my random number between 0 and 100");
+        //    playerGuess = scan.nextInt();
+        //    guessCorrect = aRandomNumber.compareNumber(playerGuess);
+        //}
+
+
+        //Requirement 7f - loop to takeTurn until game over
         //while loop to play the guessing game
-        int playerGuess;
-        boolean guessCorrect = false;
+        boolean keepPlaying = true;
 
-        while (!guessCorrect) {
-            System.out.println(myGame.person.getFullName() + ", guess my random number between 0 and 100");
-            playerGuess = scan.nextInt();
-            guessCorrect = aRandomNumber.compareNumber(playerGuess);
+        while (keepPlaying) {
+            newTurn.takeTurn(myGame.player, bobBarker);
         }
+
+        
 
 
         System.out.println("Thanks for playing!");
