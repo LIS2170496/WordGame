74e254ac26dff2becb932eed0ef2bdb5e4bc1cf8
LIS2170496 <LIS2170496@maricopa.edu>
2026-09-11 17:27:25 -0700

    fixed instantiation of newNumber

--- Hosts.java
+++ Hosts.java
@@ -7,7 +7,7 @@ public class Hosts extends Person {
 
     //Requirement 5a - instantiate Numbers.java and generate a random number
     public void randomizeNum() {
-        Numbers newNumber = new Numbers();
+        newNumber = new Numbers();
         newNumber.generateNumber();
     }
 
