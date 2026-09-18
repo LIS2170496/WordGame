2d2cafac8b7a01dbfa8dfb2f31e0cf43d8459424
LIS2170496 <LIS2170496@maricopa.edu>
2026-09-17 17:00:01 -0700

    new Award interface

---
+++ Award.java
@@ -0,0 +1,7 @@
+//Requirement 4. - Award interface
+public interface Award {
+
+    //Requirement 4a - abstract method returns int, parameters Players and boolean
+    public abstract int displayWinnings(Players playerPlayers, boolean guessCorrect);
+    
+}
