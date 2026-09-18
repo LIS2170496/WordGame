1412fc4b3e749daf6d4eef2ba94767e41877ee3e
LIS2170496 <LIS2170496@maricopa.edu>
2026-09-17 19:32:08 -0700

    new class Physical implements Award

---
+++ Physical.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+
+public class Physical implements Award {
+    
+    //Requirement 6a - String array of 5 prizes
+    String[] prizes = {
+        "new car", 
+        "Hawaii vacation", 
+        "flat screen TV", 
+        "lifetime supply of ice cream", 
+        "one-year movie theater membership"
+    };
+
+    //Requirement 6b - generate random number to assign prize
+    public int getRandomPrize() {
+        int randomPrize;
+
+        Random random = new Random();
+        randomPrize = random.nextInt(6);
+
+        return randomPrize;
+    }
+
+
+
+
+    //Requirement 6c - Implement the abstract method from Award
+    public int displayWinnings(Players playerPlayers, boolean guessCorrect) {
+
+        //Requirement 6c.i - parameter true, win a prize
+        if (guessCorrect) {
+            System.out.println("Congratulations, " + playerPlayers.getFullName() + " you guessed the number!");
+            System.out.println("You have won a " + prizes[getRandomPrize() - 1] + "!");
+            return 0;
+        }
+
+        //Requirement 6c.ii - parameter false, win nothing
+        else {
+            System.out.println("I'm sorry, " + playerPlayers.getFullName() + " you did not guess the number.");
+            System.out.println("You could have won a " + prizes[getRandomPrize() - 1] + ".");
+            return 0;
+        }
+    }
+
+
+
+
+
+
+}
