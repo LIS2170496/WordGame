a3f1eab60e0f9388b88001ab3f6844a22dd0f81c
LIS2170496 <LIS2170496@maricopa.edu>
2026-09-17 20:50:48 -0700

    takeTurn will now randomize cash or physical prize and output accordingly and update bank accordingly

--- Turn.java
+++ Turn.java
@@ -21,25 +21,61 @@ public class Turn {
         Numbers numbers = new Numbers();
 
 
-        if (numbers.compareNumber(playerGuess)) {
 
-            //Winning output and piggybank increase
-            //If player wins, add 5 times the bet amount to their piggy bank
-            playerName.setPiggyBank(playerName.getPiggyBank() + (playerName.winAmount));
-            System.out.println("Congratulations, " + playerName.getFullName() + ", you guessed the number!");
-            System.out.println(playerName.toString());
-            return true;
+        //Requirement 7b - random number to determine money or physical prize
+        //Bound 2 creates random int 0 or 1
+        //0 will be Money, 1 will be Physical
+        int moneyOrPhysical;
+        Random random = new Random();
+        moneyOrPhysical = random.nextInt(2);
+
+
+        //Requirement 7b.i - Money prize instantiates Money object
+        if (moneyOrPhysical == 0) {
+            Money cashPrize = new Money();
+
+            //moved guesses to inside this if statement
+            if (numbers.compareNumber(playerGuess)) {
+                //Winning output and piggybank increase
+                //If player wins, add 5 times the bet amount to their piggy bank
+                playerName.setPiggyBank(playerName.getPiggyBank() + 
+                    cashPrize.displayWinnings(playerName, true));
+                //System.out.println("Congratulations, " + playerName.getFullName() + ", you guessed the number!");
+                System.out.println(playerName.toString());
+                return true;
+            }
+            else {
+                //Losing output and piggybank decrease
+                //If player loses, subtract the bet amount from their piggy bank
+                playerName.setPiggyBank(playerName.getPiggyBank() + 
+                    cashPrize.displayWinnings(playerName, false));
+                //System.out.println("I'm sorry, " + playerName.getFullName() + ", you lose.");
+                System.out.println(playerName.toString());
+                return false;
+            }
         }
 
+        //Requirement 7b.ii - Physical prize instantiates Physical object
         else {
+            Physical physicalPrize = new Physical();
 
-            //Losing output and piggybank decrease
-            //If player loses, subtract the bet amount from their piggy bank
-            playerName.setPiggyBank(playerName.getPiggyBank() - playerName.betAmount);
-            //System.out.println("I'm sorry, " + playerName.getFullName() + ", you lose.");
-            System.out.println(playerName.toString());
-            return false;
-        }
+            if (numbers.compareNumber(playerGuess)) {
+                //Winning output for physical prize
+                playerName.setPiggyBank(playerName.getPiggyBank() + 
+                    physicalPrize.displayWinnings(playerName, true));
+                //System.out.println("Congratulations, " + playerName.getFullName() + ", you guessed the number!");
+                System.out.println(playerName.toString());
+                return true;
+            }
+            else {
+                //Losing output for physical prize
+                playerName.setPiggyBank(playerName.getPiggyBank() + 
+                    physicalPrize.displayWinnings(playerName, false));
+                //System.out.println("I'm sorry, " + playerName.getFullName() + ", you lose.");
+                System.out.println(playerName.toString());
+                return false;
+            }
+        } 
     }
 
 
